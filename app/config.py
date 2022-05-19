7c2c2c9e0d375dcb08c12bacd9523e0b045ecbb8
Sabrina Graham <techiepotato3004@gmail.com>
2022-05-19 13:05:38 -0400

    migrations

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY=os.environ.get('SECRET_KEY', 'some-really-long-and-unique-string')
+    SECRET_KEY=os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS=False # added just to suppress a warning
     MAIL_SERVER=os.environ.get('MAIL_SERVER')
